7feaa01ec0d4ca8e6928eaa1a7296b16f2fa16b9
Wiktor <gejzowany@gmail.com>
2021-07-12 01:25:21 +0200

    add styles and basic functionality

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
+const buttons = document.querySelectorAll(".choice");
+const playerScoreDis = document.querySelector("#playerScore");
+const computerScoreDis = document.querySelector("#computerScore");
+const outcome = document.querySelector("#outcome");
+let userScore = 0;
+let computerScore = 0;
+let userMove;
+
 computerPlay = () => {
   let moves = ["r", "p", "s"];
   return moves[Math.floor(Math.random() * 3)];
 };
-let userScore = 0;
-let computerScore = 0;
-function game() {
-  for (let i = 0; i < 5; i++) {
-    playerSelection = prompt("only give me the first letter");
-    playRound(playerSelection, computerPlay());
-    console.log(userScore + " - " + computerScore);
-  }
-}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    userMove = e.target.value;
+    playRound(userMove, computerPlay());
+  });
+});
 function playRound(player, computer) {
   switch (player + computer) {
     case "rs":
     case "pr":
     case "sp":
-      console.log("you win!");
       userScore++;
+      playerScoreDis.textContent = userScore;
       break;
     case "sr":
     case "rp":
-    case "sp":
-      console.log("you lose!");
+    case "ps":
       computerScore++;
+      computerScoreDis.textContent = computerScore;
       break;
     case "ss":
     case "rr":
     case "pp":
-      console.log("its a tie!");
       break;
   }
+  if (userScore >= 5) {
+    outcome.textContent = "You win!";
+    buttons.forEach((button) => {
+      button.removeEventListener("click");
+    });
+  } else if (computerScore >= 5) {
+    outcome.textContent = "You lose!";
+  }
 }
-game();
